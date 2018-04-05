58a25a51d71ab1b83f8e06ecfe8adba702adc83c
Kintamani <tri.wicaksono69@gmail.com>
2018-04-05 10:19:32 -0700

    Rebuild Article

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if ($request->user()->hasRole('admin')) {
+          return redirect('admin');
+        }
+        elseif ($request->user()->hasRole('user')) {
+            return redirect('user');
+        }
+        else {
+            return view('welcome');
+         }
+    }
+}
